13367e48bb230bd33ee95761758ff57a613485da
DedoDeda <91195108+DedoDeda@users.noreply.github.com>
2022-09-14 17:18:02 +0300

    Add static keyword homework.

---
+++ src/D_14_09_22/Car.java
@@ -0,0 +1,77 @@
+package D_14_09_22;
+
+public class Car {
+
+    private static int nextID;
+
+    private int ID;
+
+    private String model;
+
+    private int engineCapacity;
+
+    private int year;
+
+    private int mileage;
+
+    public Car(String model, int engineCapacity, int year, int mileage) {
+        ID = nextID;
+        nextID++;
+
+        this.model = model;
+        this.engineCapacity = engineCapacity;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "ID=" + ID +
+                ", model='" + model + '\'' +
+                ", engineCapacity=" + engineCapacity +
+                ", year=" + year +
+                ", mileage=" + mileage +
+                '}';
+    }
+}
